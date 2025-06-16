60be12358c8cb83f53011f01e6d5abf5e4abf226
90x EXE <nahid9onff@gmail.com>
2025-06-17 00:07:06 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,9 @@ logger = logging.getLogger(__name__)
 
 
 SERVERS = {
-    "BD": os.getenv("BD_SERVER", "https://clientbp.ggblueshark.com/")
+    "BD": os.getenv("BD_SERVER", "https://clientbp.ggblueshark.com/"),
+    "IND": os.getenv("IND_SERVER", "https://client.ind.freefiremobile.com"),
+    "BR": os.getenv("BR_SERVER", "https://client.us.freefiremobile.com")
     
 }
 
